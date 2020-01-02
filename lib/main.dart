2109d559088c70e6de66649db34202d859673ae0
luckypal <bigluckypal@gmail.com>
2020-01-02 12:16:34 -0600

    Make ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,233 @@
 import 'package:flutter/material.dart';
+import 'package:gplayer/gplayer.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(App());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'HLS video player',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                VideoPlayer(),
+              ],
+            ),
+          ),
+        )
+      )
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class VideoPlayer extends StatefulWidget {
+  VideoPlayer({Key key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  VideoPlayerState createState() => VideoPlayerState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class VideoPlayerState extends State<VideoPlayer> {
+  final Color controlColor = Color(0xFFF2622A);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  GPlayer player;
+
+  @override
+  void initState() {
+    super.initState();
+    //1.create & init player
+    player = GPlayer(
+      uri: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4',
+      backgroudColor: Colors.grey,
+      looping: true,
+      )
+      ..init()
+      ..addListener((params) {
+        setState(() {
+        });
+      });
+  }
+
+  @override
+  void dispose() {
+    player?.dispose();
+    super.dispose();
+  }
+
+  Widget topBarWidgets() {
+    double iconButtonHeight = MediaQuery.of(context).size.width * 0.1;
+
+    return Center(
+      child: Container(
+        height: iconButtonHeight,
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Container(),
+            ),
+            MaterialButton(
+              padding: EdgeInsets.fromLTRB(5, 0, 5, 0),
+              minWidth: 0,
+              onPressed: () {
+              },
+              child: Text("1.0 X",
+                style: TextStyle(
+                  color: controlColor,
+                ),),
+            ),
+            MaterialButton(
+              padding: EdgeInsets.fromLTRB(5, 0, 5, 0),
+              minWidth: 0,
+              onPressed: () {
+              },
+              child: Text("720P",
+                style: TextStyle(
+                  color: controlColor,
+                ),),
+            ),
+            IconButton(
+              padding: EdgeInsets.all(0),
+              icon: Icon(Icons.more_vert),
+              color: this.controlColor,
+              onPressed: () {
+              },
+            ),
+          ]
+        ),
+      ),
+    );
+  }
+
+  Widget middleWidgets() {
+    double buttonHeight = MediaQuery.of(context).size.width * 0.1;
+
+    return Container(
+      height: MediaQuery.of(context).size.width * 0.75,
+      child: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            IconButton(
+              padding: EdgeInsets.all(0),
+              icon: Icon(Icons.rotate_left),
+              color: Colors.white,
+              iconSize: buttonHeight,
+              onPressed: () {
+              },
+            ),
+            IconButton(
+              padding: EdgeInsets.all(0),
+              icon: Icon(Icons.pause),
+              color: Colors.white,
+              iconSize: buttonHeight,
+              onPressed: () {
+              },
+            ),
+            IconButton(
+              padding: EdgeInsets.all(0),
+              icon: Icon(Icons.rotate_right),
+              color: Colors.white,
+              iconSize: buttonHeight,
+              onPressed: () {
+              },
+            ),
+          ],
+        )
+      ),
+    );
+  }
+
+  Widget bottomWidgets() {
+    double buttonHeight = MediaQuery.of(context).size.width * 0.07;
+
+    return Container(
+      height: MediaQuery.of(context).size.width * 0.75,
+      child: Align(
+        alignment: Alignment.bottomCenter,
+        child: Row(
+          children: <Widget>[
+            IconButton(
+              splashColor: Colors.pink,
+              padding: EdgeInsets.all(0),
+              icon: Icon(Icons.bookmark_border),
+              color: this.controlColor,
+              iconSize: buttonHeight,
+              onPressed: () {
+              },
+            ),
+            Text(
+              "0.49",
+              style: TextStyle(
+                color: controlColor,
+              ),
+            ),
+            Expanded(
+              child: Container(
+                height: buttonHeight,
+                child: Slider(
+                  activeColor: controlColor,
+                  inactiveColor: Colors.grey,
+                  min: 0.0,
+                  max: 15.0,
+                  onChanged: (value) {
+                  },
+                  value: 5,
+                ),
+              ),
+            ),
+            Text(
+              "1.49",
+              style: TextStyle(
+                color: controlColor,
+              ),
+            ),
+            IconButton(
+              padding: EdgeInsets.all(0),
+              icon: Icon(Icons.zoom_out_map),
+              color: this.controlColor,
+              iconSize: buttonHeight,
+              onPressed: () {
+              },
+            ),
+          ],
+        )
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+    double videoHeight = MediaQuery.of(context).size.width * 0.75;
+    double buttonHeight = MediaQuery.of(context).size.width * 0.07;
+
+    return Stack(
+      children: [
+        Container(
+          color: Colors.black,
+          height: videoHeight,
+          child: player.display,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+        Container(
+          height: MediaQuery.of(context).size.width * 0.75,
+          child: Container(
+            color: Colors.black45,
+            height: MediaQuery.of(context).size.width * 0.75,
+          ),
+        ),
+
+        topBarWidgets(),
+
+        middleWidgets(),
+
+        bottomWidgets()
+      ]
     );
   }
 }
